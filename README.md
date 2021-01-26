226a977b217a17743ff5e5f8497283b0d66ffc3f
Yasir Hassan <hasanyasir01@gmail.com>
2021-01-25 23:54:29 -0800

    fetch data

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # react_shopping_cart
 Using React and Typescript to build small project for shopping cart
+
+
+to declare an object type in ts for use in React
+
+type NameOfTheObject = {
+    key: type definition like number, string , bool .. etc
+}
+
+
+to use this type we use <NameOfTheObject>
+
+now , what if we have a function that return a promise. This works when we have a async/await
+
+```js
+async function fetchData(): Promise<NameOfTheObject> {
+    return await (await fetch("url")).json 
+}
+
+```
--- cart/src/App.tsx
+++ cart/src/App.tsx
@@ -1,7 +1,50 @@
 
+import {useState , useEffect} from "react"
+import {useQuery} from "react-query"
+import axios from "axios"
 
+// components
+import {Drawer ,LinearProgress  } from "@material-ui/core"
+import Grid from "@material-ui/core/Grid"
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart" 
+import Badge from "@material-ui/core/Badge"
+
+
+// style 
+import {Wrapper} from "./App.style"
+
+// types for ts
+// this is how you declare an object in ts
+export type CartItemType = {
+  id:number,
+  category:string,
+  description:string,
+  image:string,
+  price:number,
+  title:string,
+  amount:number
+}
+
+
+// fetching data
+// we are gonna do that outside the component 
+// because we don't need to create it on each render.
+
+const getProducts = async ():Promise<CartItemType[]> => {
+  // we have 2 awaits one for the json 
+  // and the other for the api
+  return await (await fetch("https://fakestoreapi.com/products")).json()
+}
 
 function App() {
+  // the useQuery hook is a function used to register your data fetching code 
+  // into React Query Library. it takes an arbitrary key and an asynchronous function for fetching data 
+  // it returns various values 
+  const {data, isLoading , error} = useQuery<CartItemType[]>("products" , getProducts)
+
+  if(isLoading) return <LinearProgress />
+  if(error) return <h1>Something went wrong ...</h1>
+
   return (
     <div className="App">
       <h1>Hello world</h1>
