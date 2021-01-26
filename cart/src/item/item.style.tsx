7a511c8108003e0bf0c23b11cda393a351285727
Yasir Hassan <hasanyasir01@gmail.com>
2021-01-26 11:34:37 -0800

    styled the item components

--- cart/src/item/item.style.tsx
+++ cart/src/item/item.style.tsx
@@ -1,3 +1,29 @@
 import styled from "styled-components"
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+    width:100%;
+    height:100%;
+    border:1px solid lightblue;
+    border-radius:20px;
+    display: flex;
+    justify-content:space-between;
+    flex-direction:column;
+ 
+
+
+    button {
+        border-radius:0 0 20px 20px;
+    }
+
+    img {
+        max-height: 250px;
+        object-fit:cover;
+        border-radius: 20px 20px 0 0;
+    }
+
+    div {
+        font-family: Arial;
+        padding:1rem;
+        height:100%;
+    }
+`
